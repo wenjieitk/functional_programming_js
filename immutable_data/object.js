993cc3ad1457207ef15f86f1f33fcfad5bbdf55a
Wen Jie <wenjiens15@gmail.com>
2020-04-24 19:41:44 +0800

    update immutable object

--- immutable_data/object.js
+++ immutable_data/object.js
@@ -12,13 +12,24 @@ const updatedMeal = {
     calories: 600
 }
 
-console.log(updatedMeal)
-//{ id: 1, description: 'Dinner', calories: 600 }
+console.log(updatedMeal) //{ id: 1, description: 'Dinner', calories: 600 }
+
 
 /**
  * delete data without mutate original data
  */
 const {id, ...mealWithoutId} = updatedMeal
 
-console.log(mealWithoutId)
-//{ description: 'Dinner', calories: 600 }
+console.log(mealWithoutId) //{ description: 'Dinner', calories: 600 }
+
+
+/**
+ * passing data without mutate original data
+ */
+ const getMeal = ({...meal}) => {
+     meal.id = 999
+     return meal
+ }
+
+console.log(getMeal(meal)) //{ id: 999, description: 'Breakfast' }
+console.log(meal) //{ id: 1, description: 'Breakfast' }
