ae0ad3a4d2e66d182821d99a75c574862b6a7eb4
Wen Jie <wenjiens15@gmail.com>
2020-04-25 00:42:09 +0800

    array immutable data

--- immutable_data/array.js
+++ immutable_data/array.js
@@ -52,3 +52,39 @@ const ids = [1,2]
 const filteredMeals = updatedMeals.filter(meal => !ids.includes(meal.id))
 
 console.log(filteredMeals)
+
+
+
+/**
+ * test
+ */
+
+// 1. create a constant named friends, 
+// which is an array that contains 2 
+// names of your choosing.
+const friends = [
+    {name: 'jason'},
+    {name: 'ling'}
+]
+
+// 2. Create a new constant named updatedFriends, 
+// which includes the friends array values plus 
+// one additional name
+const updatedFriends = [
+    ...friends,
+    {name: 'sing'}
+]
+
+// 3. Create a new constant named friendNameLengths, 
+// which is based on the array updatedFriends, 
+// but instead of having the friends names, 
+// have the array store the length of each persons name.
+const friendNameLengths = updatedFriends.map((fren) => {
+    return fren.name.length
+})
+
+// 4. Create a new constant named shorterNamedFriends, 
+// which will be a list of the friends except the friend with the longest name.
+const maxFriendLength = Math.max(...friendNameLengths)
+const shorterNamedFriends = updatedFriends.filter(fren => fren.name.length < maxFriendLength )
+console.log(shorterNamedFriends)
