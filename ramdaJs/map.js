f8618b96699cb37599c3f69cba459d4d2c67c4cf
Wen Jie <wenjiens15@gmail.com>
2020-06-07 03:43:47 +0800

    ramda map

---
+++ ramdaJs/map.js
@@ -0,0 +1,30 @@
+const R = require('ramda')
+const cities = require('./cities.json')
+
+const KtoC = k => k - 273.15
+const KtoF = k => k * 9 / 5 - 459.67
+
+//es6 way
+const updateTemperature = (convertFn) => {
+    return city => {
+        const temp = Math.round(convertFn(city.temp))
+        return R.merge(city, {temp}) //replace 1st object element with 2nd object element
+    }
+}
+
+const updatedCities = cities.map(updateTemperature(KtoC))
+console.log(updatedCities[0])
+
+
+//Ramda way
+const updateTemperature2 = R.curry((convertFn, city) => {
+        const temp = Math.round(convertFn(city.temp))
+        return R.merge(city, {temp}) //replace 1st object element with 2nd object element
+})
+
+const updatedCities2 = cities.map(updateTemperature2(KtoC))
+console.log(updatedCities2[0])
+
+const city = cities[0]
+const updatedCity = updateTemperature2(KtoF, city)
+console.log(updatedCity)
